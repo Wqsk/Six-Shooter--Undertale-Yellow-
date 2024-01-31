c923e08b8aa7e8043ca867fcbe0a4117dc7f2848
Wqsk <weskg.weskg@gmail.com>
2024-01-31 15:52:53 -0600

    Commit 2
    Can now only select items in highlighted area

--- scripts/board.gd
+++ scripts/board.gd
@@ -3,6 +3,7 @@ extends ItemList
 var deck
 var BOARD_SIZE_LENGTH
 var BOARD_SIZE_HEIGHT
+var BOARD_SIZE
 var currentDeckAmount
 var icon
 var prevItemSelectedIndex
@@ -13,30 +14,74 @@ func setData(pileOfCards:Array[int], BOARD_LENGTH, BOARD_HEIGHT,
 	BOARD_SIZE_LENGTH = BOARD_LENGTH
 	BOARD_SIZE_HEIGHT = BOARD_HEIGHT
 	currentDeckAmount = currDeckAmount
+	BOARD_SIZE = BOARD_HEIGHT*BOARD_LENGTH
 
 func addIconItem(iconPath):
 	if iconPath != null:
 		add_icon_item(load(iconPath))
-		
-func _on_item_selected(index):
-	#	remove_item(index)
-	#	fillFromDeck(deck)
-	pass
 	
 func _on_item_clicked(index, at_position, mouse_button_index):
 	var otherSelectedItemIndex
 	var selectedItems
 	if prevItemSelectedIndex != null:
+		# User clicked same tile. Don't do anything besides unselecting.
+		if prevItemSelectedIndex == index || isHighlighted(index):
+			prevItemSelectedIndex = null
+			unhighlightAll()
+			return
+			
 		if prevItemSelectedIndex > index:
 			remove_item(prevItemSelectedIndex)
 			remove_item(index)
-		else:
+		elif index > prevItemSelectedIndex:
 			remove_item(index)
 			remove_item(prevItemSelectedIndex)
 		fillFromDeck(deck)
+		unhighlightAll()
 		prevItemSelectedIndex = null
 	else:
 		prevItemSelectedIndex = index
+		highlightNeighbors(index)
+
+func isHighlighted(index):
+	# if item at index has not changed from default, then it is not highlighted.
+	return get_item_icon_modulate(index) != Color(1,1,1)
+
+func getNeighbors(index):
+	if index == 0 || index%5 == 0:
+		return 1
+	if (index+1)%5 == 0:
+		return 2
+	else:
+		return 0
+
+func unhighlightAll():
+	for cards in range(BOARD_SIZE):
+		set_item_icon_modulate(cards, Color(1,1,1))
+		
+func highlightNeighbors(index):
+	var edgeValue = getNeighbors(index)
+	print("Edge Value: " + str(edgeValue)) #test
+	for cards in range(BOARD_SIZE):
+		# Top and Bottom
+		if cards == index || cards+5 == index || cards-5 == index:
+			pass
+		# Left and Right
+		elif cards-1 == index || cards+1 == index:
+			pass
+		# Corners
+		elif cards-6 == index || cards-4 == index || cards+6 == index ||\
+				cards+4 == index:
+			pass
+		else:
+			set_item_icon_modulate(cards, Color(0.8, 0.8, 0.8))
+			
+		if edgeValue == 1:
+			if cards == index-1 || cards == index+4 || cards == index-6:
+				set_item_icon_modulate(cards, Color(0.8, 0.8, 0.8))
+		if edgeValue == 2:
+			if cards == index+1 || cards == index-4 || cards == index+6:
+				set_item_icon_modulate(cards, Color(0.8, 0.8, 0.8))
 
 func fillFromDeck(pileOfCards):
 	if currentDeckAmount <= 0:
